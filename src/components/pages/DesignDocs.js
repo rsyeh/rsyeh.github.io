d13ae5301656dece6f6d8212bd42acef9be23f9b
Ryan Yeh <>
2018-03-13 18:22:47 -0400

    putting together the beginning of the design guide

--- src/components/pages/DesignDocs.js
+++ src/components/pages/DesignDocs.js
@@ -14,19 +14,20 @@ class DesignDocsPage extends Component {
           <div className="row">
             <div className="col-3">
               <h3 className=""> Enabled </h3>
-              <button className="full-width button--color--primary"> Primary Button </button>
+              <button className="full-width button--primary--color"> Primary Button </button>
             </div>
+            <div className="button"> Button </div>
             <div className="col-3">
               <h3 className=""> Disabled </h3>
-              <button className="full-width button--color--primary disabled"> Primary Button </button>
+              <button className="full-width button--primary--color disabled"> Primary Button </button>
             </div>
             <div className="col-3">
               <h3 className=""> Enabled </h3>
-              <button className="full-width button--color--secondary"> Secondary Button </button>
+              <button className="full-width button--primary--white"> Secondary Button </button>
             </div>
             <div className="col-3">
               <h3 className=""> Disabled </h3>
-              <button className="full-width button--color--secondary disabled"> Secondary Button </button>
+              <button className="full-width button--primary--white disabled"> Secondary Button </button>
             </div>
           </div>
           <div className="row">
@@ -58,17 +59,34 @@ class DesignDocsPage extends Component {
             </div>
             <div className="col-3">
               <h3 className=""> Button with Icon Remove </h3>
-              <button className="full-width button--remove"> Button Remove <i className="times circle outline icon" /> </button>
+              <button className="full-width button--primary--unstyled"> Button Remove <i className="times circle outline icon" /> </button>
             </div>
             <div className="col-3">
               <h3 className=""> Button with Icon Add </h3>
-              <button className="full-width button--add"> Button Add <i className="plus icon" /> </button>
+              <button className="full-width button--primary--unstyled--white"> Button Add <i className="plus icon" /> </button>
+            </div>
+          </div>
+
+        </section>
+        <section>
+          <h2 className=""> Dropdown </h2>
+          <div className="ui dropdown">
+            <div className="text">File</div>
+            <i className="dropdown icon"></i>
+            <div className="menu">
+              <div className="item">
+                Open...
+              </div>
+              <div className="item">
+                Close
+              </div>
             </div>
           </div>
 
 
-        </section>
 
+
+        </section>
 
 
 
